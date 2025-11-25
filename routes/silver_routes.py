f636bcd61e3b15a6eb11272d8e1b940034cfd726
katchitechstudio <katchitechstudio@gmail.com>
2025-11-26 00:04:02 +0300

    Delete routes/silver_routes.py

--- routes/silver_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from services.silver_service import fetch_silvers
-
-silver_bp = Blueprint("silver", __name__)
-
-@silver_bp.route("/silvers/update", methods=["GET"])
-def update_silvers():
-    ok = fetch_silvers()
-    return {"success": ok}
